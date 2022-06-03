21c6fae853173e7827b662e3d59ce4084be37a8f
dafabaelah <dafabaelah12@gmail.com>
2022-06-03 15:14:41 +0700

    fix calculate point

--- controllers/poinController.js
+++ controllers/poinController.js
@@ -48,31 +48,37 @@ const postHistoryPoinById = async (req, res, next) => {
 
         const findById = await User.findById(userId);
 
-        const calculatePoin = findById.point - point_min;
+        if(findById.point < point_min){
+            res.status(400).json({message: 'Point tidak mencukupi'});
+        }else {
+            const calculatePoin = findById.point - point_min;
+    
+            const updatePoint = await User.findByIdAndUpdate(userId, {
+                point: calculatePoin,
+            })
+    
+            // Buat menyimpan poin baru
+            const newPoin = new Poin({
+                point_min,
+                status,
+            });
+            // Berdasarkan user Id
+            const user = await User.findById(userId);
+            // menetapkan db poin sebagai pengguna di db user
+            newPoin.pengguna = user;
+            // menyimpan poin
+            await newPoin.save();
+            // menambahkan db user di field sebagai new poin
+            user.historypoint.push(newPoin);
+            // simpan user
+            await user.save();
+            res.status(201).json({
+                message: 'success',
+            })
+            
+            
+        }
 
-        const updatePoint = await User.findByIdAndUpdate(userId, {
-            point: calculatePoin,
-        })
-
-        // Buat menyimpan poin baru
-        const newPoin = new Poin({
-            point_min,
-            status,
-        });
-        // Berdasarkan user Id
-        const user = await User.findById(userId);
-        // menetapkan db poin sebagai pengguna di db user
-        newPoin.pengguna = user;
-        // menyimpan poin
-        await newPoin.save();
-        // menambahkan db user di field sebagai new poin
-        user.historypoint.push(newPoin);
-        // simpan user
-        await user.save();
-        res.status(201).json({
-            message: 'success',
-        })
-        
     } catch (error) {
         res.status(400).send(error.message);
     }
